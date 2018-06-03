513f3a684956690457e83c782776b4fa956f046f
wangrongjun <15521302230@163.com>
2018-06-04 00:27:58 +0800

    完善一些代码

--- MainPage/src/main/java/com/wangrj/main_page/controller/MainPageController.java
+++ MainPage/src/main/java/com/wangrj/main_page/controller/MainPageController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * by wangrongjun on 2018/5/29.
  */
@@ -18,7 +20,21 @@ public class MainPageController {
     @GetMapping("/hello")
     @ResponseBody
     public String hello() {
-        return "hello";
+        return "hello123";
+    }
+
+    @GetMapping("/saveSession")
+    @ResponseBody
+    public String saveSession(HttpServletRequest request, String value) {
+        request.getSession().setAttribute("testParam", value);
+        return "succeed save value: " + value;
+    }
+
+    @GetMapping("/loadSession")
+    @ResponseBody
+    public String loadSession(HttpServletRequest request) {
+        Object testParam = request.getSession().getAttribute("testParam");
+        return "testParam: " + testParam;
     }
 
 }
