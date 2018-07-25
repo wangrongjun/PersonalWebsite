58a074e6429dc8e41a61add5af863130f0b7afad
wangrongjun <15521302230@163.com>
2018-07-26 00:27:09 +0800

    研究Jenkins发布Note

--- Note/JenkinsFile.groovy
+++ Note/JenkinsFile.groovy
@@ -2,14 +2,20 @@ pipeline {
     agent any
 
     stages {
-        stage('Build Note') {
+        stage('Build') {
             steps {
-                sh "cd Note && mvn clean package spring-boot:repackage -Dmaven.test.skip=true -P prod"
+                sh 'cd Note && mvn clean package spring-boot:repackage -Dmaven.test.skip=true -P prod'
             }
         }
-        stage('Deploy Note') {
+        stage('Deploy') {
             steps {
-                sh "cp Note/target/Note-1.0-SNAPSHOT.jar /wrj/jar/Note.jar"
+                sh 'cp Note/target/Note-1.0-SNAPSHOT.jar /wrj/jar/Note.jar'
+            }
+        }
+        stage('Restart') {
+            steps {
+                sh 'ps -aux | grep Note.jar | grep -v grep | awk \'{print $2}\' | xargs kill'
+                sh 'nohup java -jar /wrj/jar/Note.jar > /wrj/log/Note.log 2>&1 &'
             }
         }
     }
