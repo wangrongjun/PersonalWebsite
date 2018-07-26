21d1a88533623bcc39b3397481a430a03dbdb308
robin.wang <robin.wang@bamboosnetworks.com>
2018-07-26 14:35:54 +0800

    研究Jenkins自动发布

--- Note/Linux发布.bat
+++ Note/Linux发布.bat
@@ -1,12 +1,11 @@
-rem Build
-mkdirs /wrj/src
-cd /wrj/src
-git clone https://github.com/wangrongjun/PersonalWebsite.git
-cd PersonalWebsite/Note
+
+rem git clone https://github.com/wangrongjun/PersonalWebsite.git
+cd /wrj/src/PersonalWebsite
+git pull
+cd Note
 mvn clean package spring-boot:repackage -Dmaven.test.skip=true -P prod
 
-rem Deploy
 ps -aux | grep Note.jar | grep -v grep | awk '{print $2}' | xargs kill
-cp target/Note-1.0-SNAPSHOT.jar /wrj/jar/Note.jar
+\cp target/Note-1.0-SNAPSHOT.jar /wrj/jar/Note.jar
 nohup java -jar /wrj/jar/Note.jar > /wrj/log/Note.log 2>&1 &
 tail -f /wrj/log/Note.log
