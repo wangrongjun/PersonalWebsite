2111e420a3f1c81bca3705332f09c62a47f42e74
robin.wang <robin.wang@bamboosnetworks.com>
2018-07-26 11:50:32 +0800

    研究Jenkins自动发布

--- Note/JenkinsFile.groovy
+++ Note/JenkinsFile.groovy
@@ -14,7 +14,8 @@ pipeline {
         }
         stage('Restart') {
             steps {
-                sh script: 'ps -aux | grep Note.jar | grep -v grep | awk \'{print $2}\' | xargs kill', returnStatus: true
+//                sh script: 'ps -aux | grep Note.jar | grep -v grep | awk \'{print $2}\' | xargs kill', returnStatus: true
+                sh 'ps -aux | grep java | xargs echo'
                 sh 'JENKINS_NODE_COOKIE=dontKillMe nohup java -jar /wrj/jar/Note.jar > /wrj/log/Note.log 2>&1 &'
             }
         }
