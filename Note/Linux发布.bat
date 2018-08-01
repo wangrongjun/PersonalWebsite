bae5b479332e7d75730865e63b3b2418560890d8
robin.wang <robin.wang@bamboosnetworks.com>
2018-08-01 18:16:05 +0800

    完善发布脚本

--- Note/Linux发布.bat
+++ Note/Linux发布.bat
@@ -3,8 +3,10 @@ cd /wrj/src/PersonalWebsite
 git pull
 cd Note
 mvn clean package spring-boot:repackage -Dmaven.test.skip=true -P prod
-
 ps -aux | grep Note.jar | grep -v grep | awk '{print $2}' | xargs kill
+
+sleep 30s
+
 \cp target/Note-1.0-SNAPSHOT.jar /wrj/jar/Note.jar
 nohup java -jar /wrj/jar/Note.jar > /wrj/log/Note.log 2>&1 &
 tail -f /wrj/log/Note.log
