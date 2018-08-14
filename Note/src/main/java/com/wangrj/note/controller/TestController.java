a7a9cc9958bfceb427a5a75c3b75d38f4084bbea
robin.wang <robin.wang@bamboosnetworks.com>
2018-08-14 22:36:11 +0800

    研究上传文件的HTTP报文格式

--- Note/src/main/java/com/wangrj/note/controller/TestController.java
+++ Note/src/main/java/com/wangrj/note/controller/TestController.java
@@ -2,8 +2,10 @@ package com.wangrj.note.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
@@ -28,6 +30,15 @@ public class TestController {
     @ResponseBody
     public String testDate(Date date) {
         return "hello, date=" + date;
+    }
+
+    @PostMapping("/testUploadFile")
+    @ResponseBody
+    public String testUploadFile(String param1, String param2, MultipartFile file1, MultipartFile file2) {
+        String s = param1 + " " + param2 + " \r\n" +
+                file1.getName() + " " + file1.getOriginalFilename() + " \r\n" +
+                file2.getName() + " " + file2.getOriginalFilename();
+        return s;
     }
 
 }
