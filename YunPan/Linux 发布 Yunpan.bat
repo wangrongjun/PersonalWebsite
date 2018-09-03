b9436bec4ee009e0e1135bcc17c904b238fbb76f
wangrongjun <15521302230@163.com>
2018-09-04 02:17:17 +0800

    重构云盘

--- YunPan/Linux 发布 Yunpan.bat
+++ YunPan/Linux 发布 Yunpan.bat
@@ -5,10 +5,11 @@ ps -aux | grep YunPan.jar | grep -v grep | awk '{print $2}' | xargs kill
 cd /wrj/src/PersonalWebsite
 git pull
 cd YunPan
-mvn clean package spring-boot:repackage -Dmaven.test.skip=true -P prod
+mvn clean package spring-boot:repackage -Dmaven.test.skip=true
 
 sleep 10s
 
 \cp target/YunPan-1.0-SNAPSHOT.jar /wrj/jar/YunPan.jar
+cd /wrj
 nohup java -jar /wrj/jar/YunPan.jar > /wrj/log/YunPan.log 2>&1 &
 tail -f /wrj/log/YunPan.log
