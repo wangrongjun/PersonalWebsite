e47a4054b23ef41e4974f87b5ede0d1719911f7e
wangrongjun <15521302230@163.com>
2018-09-19 01:56:47 +0800

    MVC架构大改造

--- Note/src/main/java/com/wangrj/note/config/WebMvcConfig.java
+++ Note/src/main/java/com/wangrj/note/config/WebMvcConfig.java
@@ -3,12 +3,15 @@ package com.wangrj.note.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.wangrj.java_lib.java_util.DateUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,9 +20,23 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    // 开启Validation校验机制
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
+    }
+
+    @Bean
+    public Converter<String, Date> dateConverter() {
+        // 一旦使用lambda表达式就报错：IllegalArgumentException:
+        // Unable to determine source type <S> and target type <T> for your Converter
+        // [com.wangrj.note.config.WebMvcConfig$$Lambda$523/834864210]; does the class parameterize those types?
+        return new Converter<String, Date>() {
+            @Override
+            public Date convert(String source) {
+                return DateUtil.toDate(source);
+            }
+        };
     }
 
     @Override
