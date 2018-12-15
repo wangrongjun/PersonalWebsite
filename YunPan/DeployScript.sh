a2ec0f52eaa43b7546e879fe031c490082a06916
wangrongjun <15521302230@163.com>
2018-12-15 22:49:53 +0800

    更新发布脚本

--- YunPan/DeployScript.sh
+++ YunPan/DeployScript.sh
@@ -15,5 +15,5 @@ sleep 10s
 
 \cp target/YunPan-1.0-SNAPSHOT.jar /wrj/jar/YunPan.jar
 cd /wrj
-nohup java -jar /wrj/jar/YunPan.jar > /wrj/log/YunPan.log 2>&1 &
+nohup java -Dserver.port=81 -jar /wrj/jar/YunPan.jar > /wrj/log/YunPan.log 2>&1 &
 tail -f /wrj/log/YunPan.log
